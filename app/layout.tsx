12fbd8eb93b4234bd83816e67cd26c13cd0acc75
baylornester28 <baylornester28@gmail.com>
2025-07-14 08:55:09 -0500

    Fix all Netlify build issues: Converted client props, updated Next config, ensured static export support

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head />
-      <body style={{ margin: 0, fontFamily: 'Arial, sans-serif', background: '#fff' }}>{children}</body>
-    </html>
-  );
-}
